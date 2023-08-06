16043da0dba50586555e417b007e9a7f7bab7a93
idoSocha <idosocha88@gmail.com>
2023-08-06 14:48:47 +0300

    update

--- backend/Utils/config.ts
+++ backend/Utils/config.ts
@@ -1,10 +1,10 @@
 class Config {
   public PORT = 4000;
-  public DB_HOST = "localhost";
+  public DB_HOST = "containers-us-west-137.railway.app";
   public DB_USERNAME = "root";
-  public DB_PASSWORD = "12345678";
-  public DATABASE = "codes";
-  public DB_PORT = 3306;
+  public DB_PASSWORD = "qbyx5ifhX9TSpcKaZLNg";
+  public DATABASE = "railway";
+  public DB_PORT = 5532;
 }
 
 const config = new Config();
