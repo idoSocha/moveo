cf7de9e90f3ad677aca69bd425423b99d1e61304
idoSocha <idosocha88@gmail.com>
2023-08-07 18:27:52 +0300

    update

--- backend/Utils/dal_mysql.ts
+++ backend/Utils/dal_mysql.ts
@@ -2,12 +2,13 @@ import mysql from "mysql2";
 import config from "./config";
 
 //creating a connection object
-const connection = mysql.createPool({
-  host: process.env.DB_HOST || "localhost",
-  user: process.env.DB_USERNAME || "root",
-  password: process.env.DB_PASSWORD || "12345678",
-  database: process.env.DATABASE || "codes",
-});
+const connection = mysql.createPool(
+  process.env.DATABASE_URL
+  // host: process.env.DB_HOST || config.DB_HOST,
+  // user: process.env.DB_USERNAME || config.DB_USERNAME,
+  // password: process.env.DB_PASSWORD || config.DB_PASSWORD,
+  // database: process.env.DATABASE || config.DATABASE,
+);
 
 const execute = (sql: string): Promise<any> => {
   return new Promise<any>((resolve, reject) => {
